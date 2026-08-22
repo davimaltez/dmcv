3bf83dccac3c0d74646e632ba3632faa4b5cead9
davimaltez <maltezdavid438@gmail.com>
2026-08-21 22:38:16 -0300

    add file redirection support for task execution in processflow

--- dmcv/main.c
+++ dmcv/main.c
@@ -14,7 +14,52 @@ typedef struct {
     char nome[64];
     char *argumentos[32];
     int quantidade;
+    
+    char arquivo_entrada[256]; 
+    char arquivo_saida[256];    
+    int modo_saida;
 }Tarefa;
+
+void aplicar_redirecionamentos(Tarefa *t) {
+
+
+    //Só mexe se tiver algo dentro
+    if (strlen(t->arquivo_entrada) > 0) {
+
+        int fd_in = open(t->arquivo_entrada, O_RDONLY);
+        
+        if (fd_in < 0) {
+            printf("Erro ao abrir arquivo de entrada");
+            fflush(stdout);
+            exit(1);
+        }
+
+        dup2(fd_in, STDIN_FILENO);
+        close(fd_in);
+    }
+
+    
+    if (t->modo_saida != 0) {
+
+        int fd_out;
+
+        if (t->modo_saida == 1) {
+
+            fd_out = open(t->arquivo_saida, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        } 
+        else {
+
+            fd_out = open(t->arquivo_saida, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        }
+        if (fd_out < 0) {
+            perror("erro ao abrir arquivo de saída");
+            fflush(stdout);
+            exit(1);
+        }
+        dup2(fd_out, STDOUT_FILENO);
+        close(fd_out);
+    }
+}
 
 int buscar_tarefa(Tarefa * tarefas, int total_tarefas, char * nome_tarefa){
 
@@ -43,6 +88,7 @@ void run_sequencial(Tarefa * tarefas, int total_tarefas, char * nome_tarefa){
             if(indice_tarefa != -1){
                 //execlp(tarefas[indice_tarefa].argumentos[indice_tarefa],tarefas[indice_tarefa].argumentos[1],NULL);
                 //execvp = mais dinâmico
+                aplicar_redirecionamentos(&tarefas[indice_tarefa]);
                 execvp(tarefas[indice_tarefa].argumentos[0], tarefas[indice_tarefa].argumentos);
                 exit(1);
             }
@@ -78,6 +124,7 @@ pid_t run_tasks_parallel(Tarefa * tarefas, int total_tarefas, char * nome_tarefa
             if(indice_tarefa != -1){
                 //execlp(tarefas[indice_tarefa].argumentos[indice_tarefa],tarefas[indice_tarefa].argumentos[1],NULL);
                 //execvp = mais dinâmico
+                aplicar_redirecionamentos(&tarefas[indice_tarefa]);
                 execvp(tarefas[indice_tarefa].argumentos[0], tarefas[indice_tarefa].argumentos);
                 exit(1);
             }
@@ -103,7 +150,7 @@ void esperar_processos(pid_t * pids, int qtd_pids){
     }
 }
 
-void run_pipe(int num_pipes, int pipes[][num_pipes], Tarefa * tarefas, int tarefas_no_pipe, int * indices_tarefas_pipe){
+void run_pipe(int num_pipes, int pipes[][2], Tarefa * tarefas, int tarefas_no_pipe, int * indices_tarefas_pipe){
 
     pid_t pids[32];
 
@@ -219,6 +266,10 @@ int main(int argc, char *argv[]){
                             
                             tarefas[total_tarefas].argumentos[i] = NULL;
                             tarefas[total_tarefas].quantidade = i;
+                            tarefas[total_tarefas].arquivo_entrada[0] = '\0';  
+                            tarefas[total_tarefas].arquivo_saida[0] = '\0';
+                            tarefas[total_tarefas].modo_saida = 0;
+
                             total_tarefas++;
                             
                             printf("Tarefa Cadastrada\n");
@@ -297,39 +348,76 @@ int main(int argc, char *argv[]){
                                 run_pipe(num_pipes, pipes, tarefas, numero_tarefas_pipe,indices_tarefas_pipe);
 
                             }
-
-                            else{
-
-                                pid_t pid = fork();
-                                if (pid < 0) { 
-                                    fprintf(stderr, "Falha na criação do processo");
-                                    return 1;
-                                }
-                                else if (pid == 0) { 
-
-                                    int indice_tarefa = buscar_tarefa(tarefas,total_tarefas,palavra);
-
-                                    if(indice_tarefa != -1){
-                                        execlp(tarefas[indice_tarefa].argumentos[indice_tarefa],tarefas[total_tarefas].argumentos[1],NULL);
-                                        exit(1);
-                                    }
-
-                                    else{
-                                        printf("Tarefa não encontrada\n");
-                                        return 1;
-                                    }
-                                }
-                                else { 
-                                    int status;
-                                    pid_t esperado = waitpid(pid, &status, 0);
-
-                                    if(esperado > 0){
-                                        printf("Processo Filho concluido\n");
-                                    }
+                            else {
+                                if (palavra == NULL) {
+                                    printf("Run oq? digite os argumentos\n");
+                                } else {
+                                    run_sequencial(tarefas, total_tarefas, palavra);
                                 }
                             }
                         }
+                    
+                    //Redirecionamento de arquivo
+
+                    else if(strcmp(palavra,"input") == 0){
+                        //ex: input ordenar nomes.txt
+                        palavra = strtok(NULL, " \t\n");
+
+                        int indice_tarefa = buscar_tarefa(tarefas,total_tarefas, palavra);
+
+                        if(indice_tarefa == -1){
+                            printf("Tarefa não encontrada");
+                        }
+                        else{
                         
+                            palavra = strtok(NULL, " \t\n");
+                            //palava = "nomes.txt"
+
+                            strcpy(tarefas[indice_tarefa].arquivo_entrada, palavra);
+                            printf("Input configurado");
+                        }
+
+                    }
+
+                    else if(strcmp(palavra,"output") == 0){
+
+                        palavra = strtok(NULL, " \t\n");
+
+                        int indice_tarefa = buscar_tarefa(tarefas,total_tarefas,palavra);
+
+                        if(indice_tarefa == -1){
+                            printf("Tarefa não encontrada");
+                        }
+                        else{
+
+                            palavra = strtok(NULL, " \t\n");
+
+                            strcpy(tarefas[indice_tarefa].arquivo_saida, palavra);
+                            tarefas[indice_tarefa].modo_saida = 1;
+                            printf("Output configurado");
+
+                        }    
+                    }
+                    else if(strcmp(palavra,"append") == 0){
+
+                        palavra = strtok(NULL, " \t\n");
+
+                        int indice_tarefa = buscar_tarefa(tarefas,total_tarefas,palavra);
+
+                        if(indice_tarefa == -1){
+                            printf("Tarefa não encontrada");
+                        }
+                        else{
+
+                            palavra = strtok(NULL, " \t\n");
+
+                            strcpy(tarefas[indice_tarefa].arquivo_saida, palavra);
+                            tarefas[indice_tarefa].modo_saida = 2;
+                            printf("Append configurado");
+
+                        }    
+                    }
+
                     }
 
                 
